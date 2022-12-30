240dc96413e29961f73bd9d338d4d0e73cd4a4d0
Igor Serebryany <igor47@moomers.org>
2022-12-30 03:16:04 -0600

    nginx

--- install.sh
+++ install.sh
@@ -4,12 +4,11 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# directory for caddy state
-mkdir -p ~igor47/services/caddy/data
-mkdir -p ~igor47/services/caddy/config
-
 # for traefik acme certs
 mkdir -p ~igor47/services/traefik/acme
+
+# nginx logs
+mkdir -p ~igor47/services/nginx/logs
 
 # make directories for host-mounted service data
 mkdir -p ~igor47/services/postgres/data
